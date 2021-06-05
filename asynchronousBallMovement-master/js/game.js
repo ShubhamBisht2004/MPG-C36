4cd48a8782844717d59ff641591e99bd16b7fe4d
Shubham Bisht <77498644+ShubhamBisht2004@users.noreply.github.com>
2021-06-05 19:53:18 +0530

    Add files via upload

---
+++ asynchronousBallMovement-master/js/game.js
@@ -0,0 +1,25 @@
+class Game {
+    constructor() {
+
+    }
+    getState() {
+        var gameStateREF = database.ref('gameState');
+        gameStateREF.on("value", function(data) {
+            gameState = data.val();
+        });
+
+    }
+
+    update(state) {
+        database.ref('/').update({
+            gameState: state
+        })
+    }
+    Start() {
+        if (gameState === 0) {
+            form = new Form();
+            form.display();
+        }
+    }
+
+}
